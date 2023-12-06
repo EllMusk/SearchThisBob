0952577dd1f8279b824ec4a1609c49e43a812783
EllMusk <149638874+EllMusk@users.noreply.github.com>
2023-12-06 19:27:14 +0000

    Update game.js

--- game.js
+++ game.js
@@ -2,73 +2,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('breakoutCanvas');
     const ctx = canvas.getContext('2d');
     
-    // Set canvas to full window size
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    // Game variables
     let ballRadius = 10;
     let x = canvas.width / 2;
     let y = canvas.height - 30;
     let dx = 2;
     let dy = -2;
-    
+
     let paddleHeight = 10;
     let paddleWidth = 75;
     let paddleX = (canvas.width - paddleWidth) / 2;
-    
-    let score = 0;
-    let bricks = [];
-    let brickRowCount = 5;
-    let brickColumnCount = 9;
-    let brickWidth = 75;
-    let brickHeight = 20;
-    let brickPadding = 10;
-    let brickOffsetTop = 30;
-    let brickOffsetLeft = 30;
 
-    // Create bricks
-    for(let c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for(let r = 0; r < brickRowCount; r++) {
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
+    // ... (Rest of your initializations)
+
+    function touchMoveHandler(e) {
+        e.preventDefault(); // Prevent scrolling when touching the canvas
+        let touchX = e.touches[0].clientX;
+        paddleX = touchX - paddleWidth / 2;
+
+        // Ensure the paddle doesn't move beyond the canvas boundaries
+        if (paddleX < 0) {
+            paddleX = 0;
+        } else if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
         }
     }
 
-    document.addEventListener("touchmove", touchMoveHandler, false);
+    canvas.addEventListener("touchmove", touchMoveHandler, { passive: false });
 
-    function touchMoveHandler(e) {
-        let touchX = e.touches[0].clientX;
-        paddleX = touchX - paddleWidth / 2;
-    }
+    // ... (Rest of your game logic)
 
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-        ctx.fillStyle = "#ff00ff"; // Pink color for the ball as in the image
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    function drawPaddle() {
-        ctx.beginPath();
-        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-        ctx.fillStyle = "#ff00ff"; // Pink color for the paddle as in the image
-        ctx.fill();
-        ctx.closePath();
-    }
-
+    // Replace the brick color logic with the appropriate colors from your image
+    // You might need to preprocess your image to determine these colors
     function drawBricks() {
+        // Example of setting brick colors
         for(let c = 0; c < brickColumnCount; c++) {
             for(let r = 0; r < brickRowCount; r++) {
                 if(bricks[c][r].status == 1) {
-                    let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-                    let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                    let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                    let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                     bricks[c][r].x = brickX;
                     bricks[c][r].y = brickY;
                     ctx.beginPath();
                     ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                    ctx.fillStyle = "#0095DD"; // The color for the bricks
+                    ctx.fillStyle = "#0095DD"; // Set this to the color of the corresponding image pixel
                     ctx.fill();
                     ctx.closePath();
                 }
@@ -76,53 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function collisionDetection() {
-        for(let c = 0; c < brickColumnCount; c++) {
-            for(let r = 0; r < brickRowCount; r++) {
-                let b = bricks[c][r];
-                if(b.status == 1) {
-                    if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                        dy = -dy;
-                        b.status = 0;
-                        score++;
-                        // Check if the game is won
-                    }
-                }
-            }
-        }
-    }
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBricks();
-        drawBall();
-        drawPaddle();
-        collisionDetection();
-
-        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-        }
-        if(y + dy < ballRadius) {
-            dy = -dy;
-        } else if(y + dy > canvas.height-ballRadius) {
-            if(x > paddleX && x < paddleX + paddleWidth) {
-                dy = -dy;
-            } else {
-                // Game over logic
-                document.location.reload();
-            }
-        }
-
-        if(paddleX < 0) {
-            paddleX = 0;
-        } else if(paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
-        }
-
-        x += dx;
-        y += dy;
-        requestAnimationFrame(draw);
-    }
+    // ... (Rest of your game logic)
 
     draw();
 });
