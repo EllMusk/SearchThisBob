0da44dd41af91b0ce81dc9808a818954e742ff32
EllMusk <149638874+EllMusk@users.noreply.github.com>
2023-12-06 19:43:47 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,54 +1,22 @@
+let score = 0;
+let highScore = localStorage.getItem('highScore') || 0;
+
 document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('breakoutCanvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const breakoutCanvas = document.getElementById('breakoutCanvas');
+    const scoreSpan = document.getElementById('score');
+    const highScoreSpan = document.getElementById('highScore');
+    
+    highScoreSpan.textContent = highScore;
 
-    // ... (rest of your initializations)
+    // Initialize your game here
+    // ...
 
-    // This is a placeholder. You will need to generate this array based on your image.
-    const brickColors = [
-        ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF'],
-        // ... more rows corresponding to your image ...
-    ];
+    window.startGame = function() {
+        breakoutCanvas.hidden = false;
+        document.getElementById('scoreBoard').hidden = false;
+        scoreSpan.textContent = score;
 
-    function touchMoveHandler(e) {
-        e.preventDefault(); // This prevents the screen from scrolling.
-        let touchX = e.touches[0].clientX;
-        paddleX = touchX - paddleWidth / 2;
-
-        // Ensure the paddle doesn't move beyond the canvas boundaries
-        if (paddleX < 0) {
-            paddleX = 0;
-        } else if (paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
-        }
-    }
-
-    canvas.addEventListener("touchmove", touchMoveHandler, { passive: false });
-
-    // ... (rest of your game logic)
-
-    function drawBricks() {
-        for (let c = 0; c < brickColumnCount; c++) {
-            for (let r = 0; r < brickRowCount; r++) {
-                if (bricks[c][r].status === 1) {
-                    let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-                    let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-                    bricks[c][r].x = brickX;
-                    bricks[c][r].y = brickY;
-                    ctx.beginPath();
-                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                    // The color is fetched from the brickColors array.
-                    ctx.fillStyle = brickColors[r] && brickColors[r][c] ? brickColors[r][c] : '#FFFFFF';
-                    ctx.fill();
-                    ctx.closePath();
-                }
-            }
-        }
-    }
-
-    // ... (rest of your game logic)
-
-    draw();
+        // Start game loop
+        // ...
+    };
 });
