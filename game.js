fdfc05023359162f1b6c896ae60f0a75620a90b3
EllMusk <149638874+EllMusk@users.noreply.github.com>
2023-12-06 19:12:46 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,85 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('breakoutCanvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 600;
 
-    let ballRadius = 10;
-    let x = canvas.width / 2;
-    let y = canvas.height - 30;
-    let dx = 2;
-    let dy = -2;
+    // Set canvas to full window size
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    let paddleHeight = 10;
-    let paddleWidth = 75;
-    let paddleX = (canvas.width - paddleWidth) / 2;
+    // ... [Rest of the initializations]
 
-    let rightPressed = false;
-    let leftPressed = false;
+    function touchMoveHandler(e) {
+        if (e.touches.length == 1) { // Only deal with one finger
+            var touch = e.touches[0]; // Get the information for finger #1
+            var touchX = touch.pageX;
 
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-
-    function keyDownHandler(e) {
-        if(e.key === "Right" || e.key === "ArrowRight") {
-            rightPressed = true;
-        } else if(e.key === "Left" || e.key === "ArrowLeft") {
-            leftPressed = true;
-        }
-    }
-
-    function keyUpHandler(e) {
-        if(e.key === "Right" || e.key === "ArrowRight") {
-            rightPressed = false;
-        } else if(e.key === "Left" || e.key === "ArrowLeft") {
-            leftPressed = false;
-        }
-    }
-
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    function drawPaddle() {
-        ctx.beginPath();
-        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        drawPaddle();
-
-        if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-        }
-        if(y + dy < ballRadius) {
-            dy = -dy;
-        } else if(y + dy > canvas.height - ballRadius) {
-            if(x > paddleX && x < paddleX + paddleWidth) {
-                dy = -dy;
-            } else {
-                // Game Over logic
+            paddleX = touchX - paddleWidth / 2;
+            if (paddleX < 0) {
+                paddleX = 0;
+            } else if (paddleX + paddleWidth > canvas.width) {
+                paddleX = canvas.width - paddleWidth;
             }
         }
-
-        if(rightPressed && paddleX < canvas.width - paddleWidth) {
-            paddleX += 7;
-        } else if(leftPressed && paddleX > 0) {
-            paddleX -= 7;
-        }
-
-        x += dx;
-        y += dy;
-        requestAnimationFrame(draw);
     }
+
+    canvas.addEventListener("touchmove", touchMoveHandler, false);
+
+    // ... [Rest of your game logic]
 
     draw();
 });
