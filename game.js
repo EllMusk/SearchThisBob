d7182d30e6e02ee458c620c7c8a1b746b84e8e84
EllMusk <149638874+EllMusk@users.noreply.github.com>
2023-12-06 19:33:31 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('breakoutCanvas');
     const ctx = canvas.getContext('2d');
-    
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    let ballRadius = 10;
-    let x = canvas.width / 2;
-    let y = canvas.height - 30;
-    let dx = 2;
-    let dy = -2;
+    // ... (rest of your initializations)
 
-    let paddleHeight = 10;
-    let paddleWidth = 75;
-    let paddleX = (canvas.width - paddleWidth) / 2;
-
-    // ... (Rest of your initializations)
+    // This is a placeholder. You will need to generate this array based on your image.
+    const brickColors = [
+        ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF'],
+        // ... more rows corresponding to your image ...
+    ];
 
     function touchMoveHandler(e) {
-        e.preventDefault(); // Prevent scrolling when touching the canvas
+        e.preventDefault(); // This prevents the screen from scrolling.
         let touchX = e.touches[0].clientX;
         paddleX = touchX - paddleWidth / 2;
 
@@ -32,22 +27,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     canvas.addEventListener("touchmove", touchMoveHandler, { passive: false });
 
-    // ... (Rest of your game logic)
+    // ... (rest of your game logic)
 
-    // Replace the brick color logic with the appropriate colors from your image
-    // You might need to preprocess your image to determine these colors
     function drawBricks() {
-        // Example of setting brick colors
-        for(let c = 0; c < brickColumnCount; c++) {
-            for(let r = 0; r < brickRowCount; r++) {
-                if(bricks[c][r].status == 1) {
+        for (let c = 0; c < brickColumnCount; c++) {
+            for (let r = 0; r < brickRowCount; r++) {
+                if (bricks[c][r].status === 1) {
                     let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
                     let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                     bricks[c][r].x = brickX;
                     bricks[c][r].y = brickY;
                     ctx.beginPath();
                     ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                    ctx.fillStyle = "#0095DD"; // Set this to the color of the corresponding image pixel
+                    // The color is fetched from the brickColors array.
+                    ctx.fillStyle = brickColors[r] && brickColors[r][c] ? brickColors[r][c] : '#FFFFFF';
                     ctx.fill();
                     ctx.closePath();
                 }
@@ -55,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // ... (Rest of your game logic)
+    // ... (rest of your game logic)
 
     draw();
 });
