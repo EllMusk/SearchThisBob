92c5e2e83d8f00939c8d9157084858b591ef8968
EllMusk <149638874+EllMusk@users.noreply.github.com>
2024-01-20 03:57:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,139 +1,162 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-const scale = 20;
-const rows = canvas.height / scale;
-const columns = canvas.width / scale;
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const scale = 20;
+    const rows = canvas.height / scale;
+    const columns = canvas.width / scale;
 
-let snake;
-let score = 0;
-let highScore = 0;
+    let snake;
+    let score = 0;
+    let highScore = 0;
 
-(function setup() {
-    snake = new Snake();
-    fruit = new Fruit();
-    fruit.pickLocation();
+    (function setup() {
+        snake = new Snake();
+        fruit = new Fruit();
+        fruit.pickLocation();
 
-    window.setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        fruit.draw();
-        snake.update();
-        snake.draw();
+        window.setInterval(() => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            fruit.draw();
+            snake.update();
+            snake.draw();
 
-        if (snake.eat(fruit)) {
-            fruit.pickLocation();
+            if (snake.eat(fruit)) {
+                fruit.pickLocation();
+            }
+
+            snake.checkCollision();
+            // You can add a score display element if needed
+
+        }, 250);
+    }());
+
+    function Snake() {
+        this.x = 0;
+        this.y = 0;
+        this.xSpeed = scale * 1;
+        this.ySpeed = 0;
+        this.total = 0;
+        this.tail = [];
+
+        this.draw = function() {
+            ctx.fillStyle = "red";
+            for (let i = 0; i < this.tail.length; i++) {
+                ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+            }
+
+            ctx.fillRect(this.x, this.y, scale, scale);
+        };
+
+        this.update = function() {
+            for (let i = 0; i < this.tail.length - 1; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+
+            this.tail[this.total - 1] = { x: this.x, y: this.y };
+
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+
+            if (this.x >= canvas.width) {
+                this.x = 0;
+            } else if (this.x < 0) {
+                this.x = canvas.width - scale;
+            }
+
+            if (this.y >= canvas.height) {
+                this.y = 0;
+            } else if (this.y < 0) {
+                this.y = canvas.height - scale;
+            }
+        };
+
+        this.changeDirection = function(direction) {
+            switch(direction) {
+                case 'Up':
+                    this.xSpeed = 0;
+                    this.ySpeed = -scale * 1;
+                    break;
+                case 'Down':
+                    this.xSpeed = 0;
+                    this.ySpeed = scale * 1;
+                    break;
+                case 'Left':
+                    this.xSpeed = -scale * 1;
+                    this.ySpeed = 0;
+                    break;
+                case 'Right':
+                    this.xSpeed = scale * 1;
+                    this.ySpeed = 0;
+                    break;
+            }
+        };
+
+        this.eat = function(fruit) {
+            if (this.x === fruit.x && this.y === fruit.y) {
+                this.total++;
+                score++;
+                if(score > highScore) {
+                    highScore = score;
+                }
+                return true;
+            }
+
+            return false;
+        };
+
+        this.checkCollision = function() {
+            for (var i = 0; i < this.tail.length; i++) {
+                if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+                    this.total = 0;
+                    this.tail = [];
+                    score = 0;
+                }
+            }
+        }
+    }
+
+    function Fruit() {
+        this.x;
+        this.y;
+
+        this.pickLocation = function() {
+            this.x = (Math.floor(Math.random() * columns) + 1) * scale;
+            this.y = (Math.floor(Math.random() * rows) + 1) * scale;
         }
 
-        snake.checkCollision();
-        document.querySelector('#score').innerText = score;
+        this.draw = function() {
+            ctx.fillStyle = "#4cafab";
+            ctx.fillRect(this.x, this.y, scale, scale);
+        }
+    }
 
-    }, 250);
-}());
+    // Touch control implementation
+    let touchStartX, touchStartY;
+    canvas.addEventListener('touchstart', function(e) {
+        touchStartX = e.changedTouches[0].screenX;
+        touchStartY = e.changedTouches[0].screenY;
+        e.preventDefault();
+    });
 
-window.addEventListener('keydown', e => {
-    const direction = e.key.replace('Arrow', '');
-    snake.changeDirection(direction);
+    canvas.addEventListener('touchmove', function(e) {
+        e.preventDefault(); // Prevent scrolling and zoom
+    });
+
+    canvas.addEventListener('touchend', function(e) {
+        const touchEndX = e.changedTouches[0].screenX;
+        const touchEndY = e.changedTouches[0].screenY;
+        const xDiff = touchStartX - touchEndX;
+        const yDiff = touchStartY - touchEndY;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) { // Horizontal swipe
+            if (xDiff > 0) { snake.changeDirection('Left'); } 
+            else { snake.changeDirection('Right'); }
+        } else { // Vertical swipe
+            if (yDiff > 0) { snake.changeDirection('Up'); } 
+            else { snake.changeDirection('Down'); }
+        }
+        e.preventDefault();
+    });
 });
-
-function Snake() {
-    this.x = 0;
-    this.y = 0;
-    this.xSpeed = scale * 1;
-    this.ySpeed = 0;
-    this.total = 0;
-    this.tail = [];
-
-    this.draw = function() {
-        ctx.fillStyle = "#FFFFFF";
-        for (let i = 0; i < this.tail.length; i++) {
-            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
-        }
-
-        ctx.fillRect(this.x, this.y, scale, scale);
-    };
-
-    this.update = function() {
-        for (let i = 0; i < this.tail.length - 1; i++) {
-            this.tail[i] = this.tail[i + 1];
-        }
-
-        this.tail[this.total - 1] = { x: this.x, y: this.y };
-
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
-
-        if (this.x > canvas.width) {
-            this.x = 0;
-        }
-
-        if (this.y > canvas.height) {
-            this.y = 0;
-        }
-
-        if (this.x < 0) {
-            this.x = canvas.width;
-        }
-
-        if (this.y < 0) {
-            this.y = canvas.height;
-        }
-    };
-
-    this.changeDirection = function(direction) {
-        switch(direction) {
-            case 'Up':
-                this.xSpeed = 0;
-                this.ySpeed = -scale * 1;
-                break;
-            case 'Down':
-                this.xSpeed = 0;
-                this.ySpeed = scale * 1;
-                break;
-            case 'Left':
-                this.xSpeed = -scale * 1;
-                this.ySpeed = 0;
-                break;
-            case 'Right':
-                this.xSpeed = scale * 1;
-                this.ySpeed = 0;
-                break;
-        }
-    };
-
-    this.eat = function(fruit) {
-        if (this.x === fruit.x && this.y === fruit.y) {
-            this.total++;
-            score++;
-            if(score > highScore) {
-                highScore = score;
-            }
-            return true;
-        }
-
-        return false;
-    };
-
-    this.checkCollision = function() {
-        for (var i = 0; i < this.tail.length; i++) {
-            if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
-                this.total = 0;
-                this.tail = [];
-                score = 0;
-            }
-        }
-    }
-}
-
-function Fruit() {
-    this.x;
-    this.y;
-
-    this.pickLocation = function() {
-        this.x = (Math.floor(Math.random() * columns - 1) + 1) * scale;
-        this.y = (Math.floor(Math.random() * rows - 1) + 1) * scale;
-    }
-
-    this.draw = function() {
-        ctx.fillStyle = "#4cafab";
-        ctx.fillRect(this.x, this.y, scale, scale);
-    }
-}
