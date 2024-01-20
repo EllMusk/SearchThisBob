763733a2af2c019cbf3fca89d24872a2af293020
EllMusk <149638874+EllMusk@users.noreply.github.com>
2024-01-20 03:47:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,185 +1,139 @@
-# Simple Snake Game in Python 3 for Beginners
-# By @TokyoEdTech
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
 
-import turtle
-import time
-import random
+let snake;
+let score = 0;
+let highScore = 0;
 
-delay = 0.1
+(function setup() {
+    snake = new Snake();
+    fruit = new Fruit();
+    fruit.pickLocation();
 
-# Score
-score = 0
-high_score = 0
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
 
-# Set up the screen
-wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
-wn.bgcolor("green")
-wn.setup(width=600, height=600)
-wn.tracer(0) # Turns off the screen updates
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
 
-# Snake head
-head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("black")
-head.penup()
-head.goto(0,0)
-head.direction = "stop"
+        snake.checkCollision();
+        document.querySelector('#score').innerText = score;
 
-# Snake food
-food = turtle.Turtle()
-food.speed(0)
-food.shape("circle")
-food.color("red")
-food.penup()
-food.goto(0,100)
+    }, 250);
+}());
 
-segments = []
+window.addEventListener('keydown', e => {
+    const direction = e.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+});
 
-# Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = scale * 1;
+    this.ySpeed = 0;
+    this.total = 0;
+    this.tail = [];
 
-# Functions
-def go_up():
-    if head.direction != "down":
-        head.direction = "up"
+    this.draw = function() {
+        ctx.fillStyle = "#FFFFFF";
+        for (let i = 0; i < this.tail.length; i++) {
+            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+        }
 
-def go_down():
-    if head.direction != "up":
-        head.direction = "down"
+        ctx.fillRect(this.x, this.y, scale, scale);
+    };
 
-def go_left():
-    if head.direction != "right":
-        head.direction = "left"
+    this.update = function() {
+        for (let i = 0; i < this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i + 1];
+        }
 
-def go_right():
-    if head.direction != "left":
-        head.direction = "right"
+        this.tail[this.total - 1] = { x: this.x, y: this.y };
 
-def move():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y + 20)
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
 
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y - 20)
+        if (this.x > canvas.width) {
+            this.x = 0;
+        }
 
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
+        if (this.y > canvas.height) {
+            this.y = 0;
+        }
 
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
+        if (this.x < 0) {
+            this.x = canvas.width;
+        }
 
-# Keyboard bindings
-wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
+        if (this.y < 0) {
+            this.y = canvas.height;
+        }
+    };
 
-# Main game loop
-while True:
-    wn.update()
+    this.changeDirection = function(direction) {
+        switch(direction) {
+            case 'Up':
+                this.xSpeed = 0;
+                this.ySpeed = -scale * 1;
+                break;
+            case 'Down':
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+                break;
+            case 'Left':
+                this.xSpeed = -scale * 1;
+                this.ySpeed = 0;
+                break;
+            case 'Right':
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+                break;
+        }
+    };
 
-    # Check for a collision with the border
-    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
-        time.sleep(1)
-        head.goto(0,0)
-        head.direction = "stop"
+    this.eat = function(fruit) {
+        if (this.x === fruit.x && this.y === fruit.y) {
+            this.total++;
+            score++;
+            if(score > highScore) {
+                highScore = score;
+            }
+            return true;
+        }
 
-        # Hide the segments
-        for segment in segments:
-            segment.goto(1000, 1000)
-        
-        # Clear the segments list
-        segments.clear()
+        return false;
+    };
 
-        # Reset the score
-        score = 0
+    this.checkCollision = function() {
+        for (var i = 0; i < this.tail.length; i++) {
+            if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+                this.total = 0;
+                this.tail = [];
+                score = 0;
+            }
+        }
+    }
+}
 
-        # Reset the delay
-        delay = 0.1
+function Fruit() {
+    this.x;
+    this.y;
 
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+    this.pickLocation = function() {
+        this.x = (Math.floor(Math.random() * columns - 1) + 1) * scale;
+        this.y = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+    }
 
-
-    # Check for a collision with the food
-    if head.distance(food) < 20:
-        # Move the food to a random spot
-        x = random.randint(-290, 290)
-        y = random.randint(-290, 290)
-        food.goto(x,y)
-
-        # Add a segment
-        new_segment = turtle.Turtle()
-        new_segment.speed(0)
-        new_segment.shape("square")
-        new_segment.color("grey")
-        new_segment.penup()
-        segments.append(new_segment)
-
-        # Shorten the delay
-        delay -= 0.001
-
-        # Increase the score
-        score += 10
-
-        if score > high_score:
-            high_score = score
-        
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
-
-    # Move the end segments first in reverse order
-    for index in range(len(segments)-1, 0, -1):
-        x = segments[index-1].xcor()
-        y = segments[index-1].ycor()
-        segments[index].goto(x, y)
-
-    # Move segment 0 to where the head is
-    if len(segments) > 0:
-        x = head.xcor()
-        y = head.ycor()
-        segments[0].goto(x,y)
-
-    move()    
-
-    # Check for head collision with the body segments
-    for segment in segments:
-        if segment.distance(head) < 20:
-            time.sleep(1)
-            head.goto(0,0)
-            head.direction = "stop"
-        
-            # Hide the segments
-            for segment in segments:
-                segment.goto(1000, 1000)
-        
-            # Clear the segments list
-            segments.clear()
-
-            # Reset the score
-            score = 0
-
-            # Reset the delay
-            delay = 0.1
-        
-            # Update the score display
-            pen.clear()
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
-
-    time.sleep(delay)
-
-wn.mainloop()
+    this.draw = function() {
+        ctx.fillStyle = "#4cafab";
+        ctx.fillRect(this.x, this.y, scale, scale);
+    }
+}
